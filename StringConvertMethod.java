2086e222551a819bbe4242c3fa8b54c642e58f7f
yashvir <yashvir.singh_cs18@gla.ac.in>
2020-05-11 23:18:20 -0700

    all the method complete

--- StringConvertMethod.java
+++ StringConvertMethod.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class StringConvertMethod {
     public static void main(String[] args) {
         //convert java String to int
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int v = Integer.parseInt(s);
+        System.out.println(v);
+        //convert int to String
+        int n = sc.nextInt();
+        String s1 = String.valueOf(n);
+        System.out.println(s1);
+        // convert String to long
+        String a = sc.next();
+        Long l = Long.valueOf(a);
+        System.out.println(l);
+        // convert long to string
+        long l1 = sc.nextLong();
+        String s2 = String.valueOf(l1);
+        System.out.println(s2);
+
+
     }
 }
